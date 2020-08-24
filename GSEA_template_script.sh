8c4847ccaa9e42643d642101fa4ba04ed57e8017
Adam C. Gower <agower@bu.edu>
2020-08-24 15:07:19 -0400

    Rewrite to accommodate GSEA 4.x
    This rewrite largely maintains backwards-compatibility with the older GSEA .jar files (versions 2.x-3.x) while accommodating the newer, script-based method of running the GSEA CLI introduced in version 4.x.
    
    * run_GSEA.qsub now takes an extra variable containing the GSEA version, e.g., "2.2.1" or "4.1.0", passed in from the bash wrapper script.
     The GSEA version is then used to determine whether the appropriate files (.jar or script) are available to start the queue job, and whether to load java 11 (which is specifically recommended for use with GSEA 4.x).
    * GSEA_template_script.sh is reorganized and simplified for readability and usability.  Also integrates some code that had previously been used only within the BU Microarray and Sequencing Resource (BUMSR).
    * Formatting changes made to all scripts (80-column max, 2-space indents)
    * Minor additional changes to postprocess_GSEA.R, to improve readability and to add PID pathway collection as a known gene set group

--- GSEA_template_script.sh
+++ GSEA_template_script.sh
@@ -1,27 +1,77 @@
 #!/bin/bash
 # Run GSEA using command-line interface
 
-##### Automatically defined variables
+# Global variables #############################################################
 
-# Project prefix, e.g., "yyyy-mm-dd_Investigator"; defaults to current working directory
+# Project prefix, e.g., "yyyy-mm-dd_Investigator"
+# Defaults to current working directory
 project_prefix="$(basename $(pwd))"
-# MSigDB version
-msigdb_version="6.0"
 # SCC project account; defaults to current active group
 project_account="$(groups | cut -f1 -d' ')"
-# Paths and filenames
-gsea_path="/restricted/projectnb/cbmhive/GSEA"
-qsub_file="$gsea_path/run_GSEA.qsub"
-project_path="/restricted/projectnb/$project_account/${project_prefix/-*/}/$project_prefix"
-param_file="$gsea_path/GSEAParameters_v$msigdb_version.txt"
 
-##### Edit the following for each pairwise comparison
+# GSEA version
+# gsea_version="4.1.0" # New script-based version, not yet working right on SCC
+gsea_version="2.2.1"   # Old .jar-based version
+# MSigDB version
+msigdb_version="7.1"
 
-# Full path to .rnk file
-rnk_file="$project_path/${project_prefix}_*.rnk"
-# Textual description of comparison, e.g., "knockout vs wildtype"
-comparison=""
-# Name for the queue job/output log
-job_name="GSEA_$(basename ${rnk_file/.rnk/})"
-# Run GSEA using specified variables
-qsub -P $project_account -N $job_name -o $job_name.log $qsub_file $rnk_file $param_file "$comparison"
+# By default, look in current working directory for .rnk files; change as needed
+rnk_path="."
+
+# Set environment variable to path to GSEA files; this will be passed to the
+# queue job with the -V qsub flag so that it can find the GSEA executables
+export GSEA_PATH="/rprojectnb/cbmhive/GSEA"
+# Define filenames
+qsub_file="${GSEA_PATH}/run_GSEA.qsub"
+param_file="${GSEA_PATH}/GSEAParameters_v${msigdb_version}.txt"
+
+# Regular expressions for phrases that should be removed from .rnk filenames
+# in order to extract a textual description of the pairwise comparison
+comparison_regexes=(
+  "^${project_prefix}_"
+  "_[Mm]oderated_t$" "_Student_t$" "_t$" "_Wald_statistic$" "_log2_fold_change$"
+)
+
+# Define the main effects in the model if interaction effect was used for GSEA
+# For example:
+# effects=("genotype" "treatment")
+effects=()
+
+# Submit all *.rnk files in specified directory ################################
+
+# Resolve path name
+rnk_path="$(realpath ${rnk_path})"
+
+for rnk_file in "${rnk_path}"/*.rnk
+do
+  # Extract prefix from .rnk filename
+  rnk_file_prefix="$(echo "$(basename ${rnk_file})" | sed -r 's/\.rnk$//')"
+
+  # Extract textual description of comparison (e.g., "knockout vs wildtype")
+  # from .rnk filename prefix
+  comparison="${rnk_file_prefix}"
+  # Remove any specified phrases
+  for regex in ${comparison_regexes[@]}
+  do
+    comparison="$(echo "${comparison}" | sed "s/${regex}//")"
+  done
+  # Replace any underscores with spaces
+  comparison="$(echo "${comparison}" | sed "s/_/ /g")"
+  # An interaction effect is labeled differently
+  # Note that this code still works even if the 'effects' array is not defined
+  if [[ "${comparison}" = "${effects[@]}" ]]
+  then
+    comparison="interaction of ${effects[0]} and ${effects[1]}"
+  fi
+
+  # Run GSEA using specified variables
+  # Note: this job is run in parallel with 8 processors
+  #       since it will use more than 1 processor of power otherwise,
+  #       and the process reaper will kill it.
+  # (This was originally set to 4 processors, but the process reaper killed two
+  #  runs each using > 5 CPUs.) 2016-06-06 ACG
+  job_name="GSEA_${rnk_file_prefix}"
+  qsub -P ${project_account} -N ${job_name} -o ${job_name}.log -pe omp 8 -V \
+       "${qsub_file}" \
+       ${gsea_version} "${rnk_file}" "${param_file}" "${comparison}"
+done
